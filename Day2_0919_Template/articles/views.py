bd40a7d2d32eb3a0784ae0f22661420c36932352
garam0107 <name3937@gmail.com>
2024-09-19 09:47:55 +0900

    0919

---
+++ Day2_0919_Template/articles/views.py
@@ -0,0 +1,20 @@
+import random
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context = {
+        'name' : 'alice',
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def dinner(request):
+    foods = ['국밥', '국수', '카레', '탕수육',]
+    picked = random.choice(foods)
+    context = {
+        'foods' : foods,
+        'picked' : picked,
+    }
+    return render(request, 'articles/dinner.html', context)
