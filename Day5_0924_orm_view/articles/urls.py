9de1503901a2ca347bdb725b7bd835253dae0ab8
garam0107 <name3937@gmail.com>
2024-09-24 09:37:42 +0900

    0924

--- Day5_0924_orm_view/articles/urls.py
+++ Day5_0924_orm_view/articles/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>/', views.detail, name = 'detail'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- Day5_0924_orm_view/articles/views.py
+++ Day5_0924_orm_view/articles/views.py
@@ -10,3 +10,32 @@ def index(request):
         'articles': articles,
     }
     return render(request, 'articles/index.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    # 1. 사용자 요청으로부터 입력 데이터를 추출
+    # 2. 추출한 입력 데이터를 활용해 DB에 저장 요청
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    # 데이터 저장 3가지 방법
+    # 1
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    # 2
+    article = Article(title = title ,content = content)
+    article.save()
+
+    #3
+    # Article.objects.create(title = title ,content = content)
