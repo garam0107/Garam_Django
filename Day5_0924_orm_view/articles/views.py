6cdcf8a468c7c31a565ab9fc530d68994d997c6c
garam0107 <name3937@gmail.com>
2024-09-24 11:00:32 +0900

    0924

--- Day5_0924_orm_view/articles/views.py
+++ Day5_0924_orm_view/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # 모델 클래스 가져오기
 from .models import Article
 
@@ -24,8 +24,8 @@ def new(request):
 def create(request):
     # 1. 사용자 요청으로부터 입력 데이터를 추출
     # 2. 추출한 입력 데이터를 활용해 DB에 저장 요청
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
     # 데이터 저장 3가지 방법
     # 1
     # article = Article()
@@ -36,6 +36,35 @@ def create(request):
     # 2
     article = Article(title = title ,content = content)
     article.save()
-
+    # 유효성 검사와 가독성이 좋은 2번 방법을 주로 선택
     #3
     # Article.objects.create(title = title ,content = content)
+    return redirect('articles:detail', article.pk)
+    # return redirect('articles:index')
+def delete(request, pk):
+    article = Article.objects.get(pk = pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request,pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    # 1. 어떤 게시글 수정할지 조회
+    article = Article.objects.get(pk=pk)
+    # 2. 사용자로부터 받은 새로운 입력 데이터 추출
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    # 3. 기존 게시글 데이터를 사용자로 받은 데이터로 새로 할당
+    article.title = title
+    article.content = content
+    # 4. 저장
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
+    
