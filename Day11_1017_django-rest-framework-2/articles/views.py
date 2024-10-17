13bf11b4290f1ce5606672fbeb654d5c37003329
garam0107 <name3937@gmail.com>
2024-10-17 10:52:56 +0900

    1017_live

--- Day11_1017_django-rest-framework-2/articles/views.py
+++ Day11_1017_django-rest-framework-2/articles/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-# from django.shortcuts import get_list_or_404,get_object_or_404
+from django.shortcuts import get_list_or_404,get_object_or_404
 from .models import Article,Comment
 from .serializers import ArticleListSerializer, ArticleSerializer,CommentSerializer
 
@@ -9,7 +9,8 @@ from .serializers import ArticleListSerializer, ArticleSerializer,CommentSeriali
 @api_view(['GET', 'POST'])
 def article_list(request):
     if request.method == 'GET':
-        articles = Article.objects.all()
+        # articles = Article.objects.all()
+        articles = get_list_or_404(Article)
         serializer = ArticleListSerializer(articles, many=True)
         return Response(serializer.data)
 
@@ -22,8 +23,8 @@ def article_list(request):
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def article_detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk = article_pk)
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
@@ -41,12 +42,14 @@ def article_detail(request, article_pk):
             return Response(serializer.data)
 @api_view(['GET'])
 def comments(request):
-    comment = Comment.objects.all()
+    # comment = Comment.objects.all()
+    comment = get_list_or_404(Comment)
     serializer = CommentSerializer(comment, many =True)
     return Response(serializer.data)
 @api_view(['GET','PUT','DELETE'])
 def comment_detail(request,comment_pk):
-    comment = Comment.objects.get(pk = comment_pk)
+    # comment = Comment.objects.get(pk = comment_pk)
+    comment = get_object_or_404(Comment, pk=comment_pk)
     if request.method == 'GET':
         serializer = CommentSerializer(comment)
         return Response(serializer.data)
@@ -63,7 +66,8 @@ def comment_detail(request,comment_pk):
 
 @api_view(['POST'])
 def comment_create(request,article_pk):
-    article = Article.objects.get(pk = article_pk)
+    # article = Article.objects.get(pk = article_pk)
+    article = get_object_or_404(Article, pk = article_pk)
     serializer = CommentSerializer(data = request.data)
     if serializer.is_valid(raise_exception=True):
         serializer.save(article = article)
